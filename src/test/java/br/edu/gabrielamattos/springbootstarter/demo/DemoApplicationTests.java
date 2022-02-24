d1c040c0f1e3f3b5aed320df27ecc198b3e1963a
Gabriela Mattos <gabrielamattosde@gmail.com>
2022-02-24 12:35:23 -0300

    Iniciando projeto de estudo do Spring

---
+++ src/test/java/br/edu/gabrielamattos/springbootstarter/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.gabrielamattos.springbootstarter.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
